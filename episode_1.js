fcfc26e5d9453b7cdd325ac6048738485af2d9f0
Duncan Ryan <duncancryan1@gmail.com>
2020-08-18 00:07:28 +0100

    had to remove ep 2 from ep 1

--- episode_1.js
+++ episode_1.js
@@ -14,20 +14,3 @@ console.log(verdict);
 // I think the output will be "The murderer is Miss Scarlet."
 // this seems pretty unproblematic when it comes to scope 
 // there are no variables defined inside the functions that will not be able to be accessed etc.
-
-const murderer = 'Professor Plum';
-
-const changeMurderer = function () {
-    murderer = 'Mrs. Peacock';
-}
-
-const declareMurderer = function () {
-    return `The murderer is ${murderer}.`;
-}
-
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
-
-// I think that the verdict will be "The murderer is Professor Plum."
-// murderer is defined as a const here and thus cannot be reassigned, even by a function
